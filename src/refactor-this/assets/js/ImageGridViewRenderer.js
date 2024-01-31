59584f4f0d20df43db82f2e6a9b9a69ab2e9956c
fredrekt <fredrickjohng6@gmail.com>
2024-02-01 06:25:15 +0800

    refactored: readability, resuability, maintainable and etc

--- src/refactor-this/assets/js/ImageGridViewRenderer.js
+++ src/refactor-this/assets/js/ImageGridViewRenderer.js
@@ -1,21 +1,21 @@
 function ImageGridViewRenderer() {}
 
 ImageGridViewRenderer.prototype.render = function () {
-  var nav =
-   '<nav class="navbar navbar-expand-lg navbar-light bg-light">'
-  +'  <a class="navbar-brand" href="#">Photo Sharing App</a>'
-  +'  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">'
-  +'    <span class="navbar-toggler-icon"></span>'
-  +'  </button>'
-  +'  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">'
-  +'    <div class="navbar-nav">'
-  +'      <a class="nav-link active" href="?nature">Nature <span class="sr-only">(current)</span></a>'
-  +'      <a class="nav-link" href="?architecture">Architecture</a>'
-  +'      <a class="nav-link" href="?fashion">Fashion</a>'
-  +'    </div>'
-  +'  </div>'
-  +'</nav>'
- 
+  var nav = `
+    <nav id="navbar" class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="/">Photo Sharing App</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link ${window.location.search.includes('?nature') ? 'active' : ''}" href="?nature">Nature <span class="sr-only">(current)</span></a>
+          <a class="nav-link ${window.location.search.includes('?architecture') ? 'active' : ''}" href="?architecture">Architecture</a>
+          <a class="nav-link ${window.location.search.includes('?fashion') ? 'active' : ''}" href="?fashion">Fashion</a>
+        </div>
+      </div>
+    </nav>`;
+
   document.getElementById("main-view").innerHTML = nav
 
   var page = 1
@@ -24,139 +24,125 @@ ImageGridViewRenderer.prototype.render = function () {
   }
 
   if (window.location.search.includes('?nature')) {
-    document.getElementById("main-view").innerHTML +=
-     '<div class="container">'
-    +'  <div id="nature-images" class="row row-cols-3"></div>'
-    +'</div>'
+    const containerId = 'nature-images'
+    document.getElementById("main-view").innerHTML += generateGridContainerImage(containerId);
     ImageDataGetter.getNatureImagesFromPage((page * 3) - 2)
       .then(function (images) {
         for (var i = 0; i < images.length; i++) {
-          document.getElementById("nature-images").innerHTML +=
-          '<div class="col" style="height: 400px; padding: 10px;">'
-          +'  <img class="image" src="' + images[i].url + '" alt="' + images[i].name + '" style="height: 100%; object-fit: cover; width: 100%;" />'
-          +'  <div class="middle">'
-          +'    <a class="btn btn-dark" href="' + images[i].url + '" download="' + images[i].name + '">DOWNLOAD</a>'
-          +'  </div>'
-          +'</div>'
+          document.getElementById(containerId).innerHTML += generateGridImages(images[i])
         }
       })
     ImageDataGetter.getNatureImagesFromPage((page * 3) - 1)
       .then(function (images) {
         for (var i = 0; i < images.length; i++) {
-          document.getElementById("nature-images").innerHTML +=
-          '<div class="col" style="height: 400px; padding: 10px;">'
-          +'  <img class="image" src="' + images[i].url + '" alt="' + images[i].name + '" style="height: 100%; object-fit: cover; width: 100%;" />'
-          +'  <div class="middle">'
-          +'    <a class="btn btn-dark" href="' + images[i].url + '" download="' + images[i].name + '">DOWNLOAD</a>'
-          +'  </div>'
-          +'</div>'
+          document.getElementById(containerId).innerHTML += generateGridImages(images[i])
         }
       })
     ImageDataGetter.getNatureImagesFromPage(page * 3)
       .then(function (images) {
         for (var i = 0; i < images.length; i++) {
-          document.getElementById("nature-images").innerHTML +=
-          '<div class="col" style="height: 400px; padding: 10px;">'
-          +'  <img class="image" src="' + images[i].url + '" alt="' + images[i].name + '" style="height: 100%; object-fit: cover; width: 100%;" />'
-          +'  <div class="middle">'
-          +'    <a class="btn btn-dark" href="' + images[i].url + '" download="' + images[i].name + '">DOWNLOAD</a>'
-          +'  </div>'
-          +'</div>'
+          document.getElementById(containerId).innerHTML += generateGridImages(images[i])
         }
       })
   } else if (window.location.search.includes('?architecture')) {
-    document.getElementById("main-view").innerHTML +=
-     '<div class="container">'
-    +'  <div id="architecture-images" class="row row-cols-3"></div>'
-    +'</div>'
+    const containerId = "architecture-images";
+    document.getElementById("main-view").innerHTML += generateGridContainerImage(containerId);
     ImageDataGetter.getArchitectureImagesFromPage((page * 3) - 2)
       .then(function (images) {
         for (var i = 0; i < images.length; i++) {
-          document.getElementById("architecture-images").innerHTML +=
-          '<div class="col" style="height: 400px; padding: 10px;">'
-          +'  <img class="image" src="' + images[i].url + '" alt="' + images[i].name + '" style="height: 100%; object-fit: cover; width: 100%;" />'
-          +'  <div class="middle">'
-          +'    <a class="btn btn-dark" href="' + images[i].url + '" download="' + images[i].name + '">DOWNLOAD</a>'
-          +'  </div>'
-          +'</div>'
+          document.getElementById(containerId).innerHTML += generateGridImages(images[i])
         }
       })
     ImageDataGetter.getArchitectureImagesFromPage((page * 3) - 1)
       .then(function (images) {
         for (var i = 0; i < images.length; i++) {
-          document.getElementById("architecture-images").innerHTML +=
-          '<div class="col" style="height: 400px; padding: 10px;">'
-          +'  <img class="image" src="' + images[i].url + '" alt="' + images[i].name + '" style="height: 100%; object-fit: cover; width: 100%;" />'
-          +'  <div class="middle">'
-          +'    <a class="btn btn-dark" href="' + images[i].url + '" download="' + images[i].name + '">DOWNLOAD</a>'
-          +'  </div>'
-          +'</div>'
+          document.getElementById(containerId).innerHTML += generateGridImages(images[i])
         }
       })
     ImageDataGetter.getArchitectureImagesFromPage(page * 3)
       .then(function (images) {
         for (var i = 0; i < images.length; i++) {
-          document.getElementById("architecture-images").innerHTML +=
-          '<div class="col" style="height: 400px; padding: 10px;">'
-          +'  <img class="image" src="' + images[i].url + '" alt="' + images[i].name + '" style="height: 100%; object-fit: cover; width: 100%;" />'
-          +'  <div class="middle">'
-          +'    <a class="btn btn-dark" href="' + images[i].url + '" download="' + images[i].name + '">DOWNLOAD</a>'
-          +'  </div>'
-          +'</div>'
+          document.getElementById(containerId).innerHTML += generateGridImages(images[i])
         }
       })
   } else if (window.location.search.includes('?fashion')) {
-    document.getElementById("main-view").innerHTML +=
-     '<div class="container">'
-    +'  <div id="fashion-images" class="row row-cols-3"></div>'
-    +'</div>'
+    const containerId = "fashion-images";
+    document.getElementById("main-view").innerHTML += generateGridContainerImage(containerId);
     ImageDataGetter.getFashionImagesFromPage((page * 3) - 2)
       .then(function (images) {
         for (var i = 0; i < images.length; i++) {
-          document.getElementById("fashion-images").innerHTML +=
-          '<div class="col" style="height: 400px; padding: 10px;">'
-          +'  <img class="image" src="' + images[i].url + '" alt="' + images[i].name + '" style="height: 100%; object-fit: cover; width: 100%;" />'
-          +'  <div class="middle">'
-          +'    <a class="btn btn-dark" href="' + images[i].url + '" download="' + images[i].name + '">DOWNLOAD</a>'
-          +'  </div>'
-          +'</div>'
+          document.getElementById(containerId).innerHTML += generateGridImages(images[i])
         }
       })
     ImageDataGetter.getFashionImagesFromPage((page * 3) - 1)
       .then(function (images) {
         for (var i = 0; i < images.length; i++) {
-          document.getElementById("fashion-images").innerHTML +=
-          '<div class="col" style="height: 400px; padding: 10px;">'
-          +'  <img class="image" src="' + images[i].url + '" alt="' + images[i].name + '" style="height: 100%; object-fit: cover; width: 100%;" />'
-          +'  <div class="middle">'
-          +'    <a class="btn btn-dark" href="' + images[i].url + '" download="' + images[i].name + '">DOWNLOAD</a>'
-          +'  </div>'
-          +'</div>'
+          document.getElementById(containerId).innerHTML += generateGridImages(images[i])
         }
       })
     ImageDataGetter.getFashionImagesFromPage(page * 3)
       .then(function (images) {
         for (var i = 0; i < images.length; i++) {
-          document.getElementById("fashion-images").innerHTML +=
-          '<div class="col" style="height: 400px; padding: 10px;">'
-          +'  <img class="image" src="' + images[i].url + '" alt="' + images[i].name + '" style="height: 100%; object-fit: cover; width: 100%;" />'
-          +'  <div class="middle">'
-          +'    <a class="btn btn-dark" href="' + images[i].url + '" download="' + images[i].name + '">DOWNLOAD</a>'
-          +'  </div>'
-          +'</div>'
+          document.getElementById(containerId).innerHTML += generateGridImages(images[i])
         }
       })
+  } else {
+    const containerId = "default-images";
+    document.getElementById("main-view").innerHTML += generateGridContainerImage(containerId);
+    ImageDataGetter.getFashionImagesFromPage((page * 3) - 2)
+      .then(function (images) {
+        for (var i = 0; i < images.length; i++) {
+          document.getElementById(containerId).innerHTML += generateGridImages(images[i])
+        }
+      })
+    ImageDataGetter.getArchitectureImagesFromPage((page * 3) - 1)
+    .then(function (images) {
+      for (var i = 0; i < images.length; i++) {
+        document.getElementById(containerId).innerHTML += generateGridImages(images[i])
+      }
+    })
+    ImageDataGetter.getNatureImagesFromPage(page * 3)
+    .then(function (images) {
+      for (var i = 0; i < images.length; i++) {
+        document.getElementById(containerId).innerHTML += generateGridImages(images[i])
+      }
+    })
+  }
+
+  function generateGridContainerImage(containerId) {
+    return `
+      <div class="container">
+        <div id="${containerId}" class="row row-cols-3"></div>
+      </div>`;
+  }
+
+  function generateGridImages(image) {
+    return (`
+      <div class="col" style="height: 400px; padding: 10px;">
+        <img class="image" src="${image.url}" alt="${image.name}" style="height: 100%; object-fit: cover; width: 100%;" />
+        <div class="middle">
+          <a class="btn btn-dark" href="${image.url}" download="${image.name}">DOWNLOAD</a>
+        </div>
+      </div>
+    `);
   }
 
   var prevsearchstr = window.location.search.split('&page')[0] + '&page=' + (page - 1)
   var nextsearchstr = window.location.search.split('&page')[0] + '&page=' + (page + 1)
-  var pagination =
-   '<nav>'
-  +'  <ul class="pagination">'
-  +'    <li class="page-item"><a class="page-link" href="' + prevsearchstr + '">Previous</a></li>'
-  +'    <li class="page-item"><a class="page-link" href="' + nextsearchstr + '">Next</a></li>'
-  +'  </ul>'
-  +'</nav>'
+  const pagination = `
+  <nav id="pagination-nav" class="container mt-4 px-2">
+    <ul class="pagination">
+      <li class="page-item"><a title='Previous' class="page-link black-text" href="${prevsearchstr}"><i class="bi bi-arrow-left"></i></a></li>
+      <li class="page-item"><a title='Next' class="page-link black-text" href="${nextsearchstr}"><i class="bi bi-arrow-right"></i></a></li>
+    </ul>
+  </nav>`;
 
   document.getElementById("main-view").innerHTML += pagination
+
+  const paginationNav = document.getElementById("pagination-nav");
+  if (window.location.pathname === '/' && window.location.search === '') {
+    paginationNav.style.display = 'none';
+  } else {
+    paginationNav.style.display = 'block';
+  }
 }
